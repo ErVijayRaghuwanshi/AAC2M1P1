e430e4b55a6fce946cb84cadc52d94f213d31c54
Vijay Raghuwanshi <gvijay745@gmail.com>
2020-11-24 15:08:38 +0530

    Complete all Week1 Assignment

--- AAC2M1P1.vhd
+++ AAC2M1P1.vhd
@@ -37,14 +37,14 @@
 library ieee;                                	
 use ieee.std_logic_1164.all;           	
                                              		
-entity Comparator2 is port (                 	
+entity comparator2 is port (                 	
     A, B: in std_logic_vector(1 downto 0); 
     Equals: out std_logic);            		
-end Comparator2;                             	
+end comparator2;                             	
 
 -- Architecture
 -- Boolean logic descrition
-architecture bool of Comparator2 is
+architecture bool of comparator2 is
     begin
         Equals <= not(A(1) xor B(1)) and
                   not(A(0) xor B(0));
